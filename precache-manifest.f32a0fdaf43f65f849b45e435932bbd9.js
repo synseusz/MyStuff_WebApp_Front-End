5b3123a7a4e30c19aa557b78a418b1af07682caf
synseusz <thesynseusz@gmail.com>
2020-06-03 20:43:36 +0100

    Updates

---
+++ precache-manifest.f32a0fdaf43f65f849b45e435932bbd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16120f43adc7c9a33659422d3b13c110",
+    "url": "/MyStuff_WebApp_Front-End/static/media/CU_Logo.16120f43.png"
+  },
+  {
+    "revision": "b1ba41a6b3c843e0a5bc",
+    "url": "/MyStuff_WebApp_Front-End/static/js/runtime~main.b1ba41a6.js"
+  },
+  {
+    "revision": "de44fe5535701f800654",
+    "url": "/MyStuff_WebApp_Front-End/static/js/main.de44fe55.chunk.js"
+  },
+  {
+    "revision": "88e92fb30b61f29969ec",
+    "url": "/MyStuff_WebApp_Front-End/static/js/1.88e92fb3.chunk.js"
+  },
+  {
+    "revision": "de44fe5535701f800654",
+    "url": "/MyStuff_WebApp_Front-End/static/css/main.d81e8045.chunk.css"
+  },
+  {
+    "revision": "3562715ce3866d5e8f419699a27674d3",
+    "url": "/MyStuff_WebApp_Front-End/index.html"
+  }
+];
